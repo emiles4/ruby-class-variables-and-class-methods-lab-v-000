c947d296f55ef211d3351f07639a08c721fab700
Emily Miles <emiles4@gmail.com>
2019-03-28 16:18:40 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,32 @@
+require 'pry'
+
+class Song
+  attr_accessor :name; :artist; :genre
+  attr_reader :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize (name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+  @@count+=1
+  @@artists << artist
+  @@genres << genre
+  end
+
+  def self.artists
+  @@artist.uniq
+  end
+
+  def self.count
+    @@count
+  end
+
+  def genres
+    genre.uniq
+  end
+
+end
